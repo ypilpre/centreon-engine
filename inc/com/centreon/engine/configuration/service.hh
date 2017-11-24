1b77e2c5a3b1c91bb7b656ebd345e1ba2bc22543
David Boucher <dboucher@centreon.com>
2017-11-24 10:52:33 +0100

    cleanup(configuration/service): recovery flag added

--- inc/com/centreon/engine/configuration/service.hh
+++ inc/com/centreon/engine/configuration/service.hh
@@ -44,7 +44,8 @@ namespace                  configuration {
       unknown = (1 << 2),
       critical = (1 << 3),
       flapping = (1 << 4),
-      downtime = (1 << 5)
+      downtime = (1 << 5),
+      recovery = (1 << 6),
     };
     typedef                std::pair<std::string, std::string>
                            key_type;
