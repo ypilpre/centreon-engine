809f8a046cb4c251af2af3171aaf3904abb865b8
David Boucher <dboucher@centreon.com>
2017-11-24 10:54:15 +0100

    cleanup(engine/contactgroup): contactgroup refactored.

--- inc/com/centreon/engine/contactgroup.hh
+++ inc/com/centreon/engine/contactgroup.hh
@@ -20,26 +20,59 @@
 #ifndef CCE_CONTACTGROUP_HH
 #  define CCE_CONTACTGROUP_HH
 
-#include "com/centreon/engine/namespace.hh"
+#  include "com/centreon/engine/namespace.hh"
+#  include "com/centreon/shared_ptr.hh"
+#  include "com/centreon/unordered_hash.hh"
 
 CCE_BEGIN()
+
+// Forward declaration
+class contact;
+
+namespace configuration {
+  class contactgroup;
+}
 
   /**
    *  @class contactgroup contactgroup.hh "com/centreon/engine/contactgroup.hh
    *  @brief Object representing a contactgroup user
    *
    */
-class               contactgroup {
+class                          contactgroup {
  public:
-                    contactgroup();
-                    contactgroup(contactgroup const& other);
-  virtual           ~contactgroup();
-  contactgroup&     operator=(contactgroup const& other);
-  std::string&      get_name() const;
-  bool              check(int* w, int* e);
+  static contactgroup*         add_contactgroup(
+                                 char const* name,
+                                 char const* alias);
+                               contactgroup(
+                                 char const* name,
+                                 char const* alias);
+                               contactgroup();
+                               contactgroup(contactgroup const& other);
+  virtual                      ~contactgroup();
+  contactgroup&                operator=(contactgroup const& other);
+  bool                         operator<(contactgroup const& other) const;
+  bool                         operator!=(contactgroup const& other) const;
+  std::string const&           get_name() const;
+  std::string const&           get_alias() const;
+  bool                         check(int* w, int* e);
+  umap<std::string, com::centreon::shared_ptr<contact> > const&
+                               get_members() const;
+
+  umap<std::string, com::centreon::shared_ptr<contact> >&
+                               get_members();
+
+  bool                         contains_member(std::string const& name) const;
+  bool                         contains_illegal_object_chars() const;
+  void                         add_contact(std::string const& contact_name);
+  void                         update_config(
+                                 configuration::contactgroup const& obj);
+  void                         clear_members();
 
  private:
-  std::string       _name;
+  std::string                  _name;
+  std::string                  _alias;
+  umap<std::string, com::centreon::shared_ptr<contact> >
+                               _members;
 };
 
 CCE_END()
--- src/contactgroup.cc
+++ src/contactgroup.cc
@@ -19,11 +19,20 @@
 
 #include <memory>
 #include <sstream>
-#include "com/centreon/engine/contactgroup.hh"
+#include "com/centreon/engine/broker.hh"
+#include "com/centreon/engine/configuration/applier/object.hh"
+#include "com/centreon/engine/configuration/applier/state.hh"
 #include "com/centreon/engine/configuration/contactgroup.hh"
+#include "com/centreon/engine/contact.hh"
+#include "com/centreon/engine/contactgroup.hh"
+#include "com/centreon/engine/error.hh"
+#include "com/centreon/engine/globals.hh"
+#include "com/centreon/engine/logging/logger.hh"
+#include "find.hh"
 
 using namespace com::centreon;
 using namespace com::centreon::engine;
+using namespace com::centreon::engine::logging;
 
 
 /**************************************                                         
@@ -33,9 +42,72 @@ using namespace com::centreon::engine;
 **************************************/                                         
 
 /**
+ *  Add a new contact group to the list in memory.
+ *
+ *  @param[in] name  Contact group name.
+ *  @param[in] alias Contact group alias.
+ */
+contactgroup* contactgroup::add_contactgroup(
+                              char const* name,
+                              char const* alias) {
+  // Make sure we have the data we need.
+  if (!name || !name[0]) {
+    logger(log_config_error, basic)
+      << "Error: Contactgroup name is NULL";
+    return (NULL);
+  }
+
+  // Check if the contact group already exist.
+  umap<std::string, shared_ptr<contactgroup> >::const_iterator
+    it(configuration::applier::state::instance().contactgroups().find(name));
+  if (it == configuration::applier::state::instance().contactgroups().end()) {
+    logger(log_config_error, basic)
+      << "Error: Contactgroup '" << name << "' has already been defined";
+    return (NULL);
+  }
+
+  // Allocate memory for a new contactgroup.
+  shared_ptr<contactgroup> obj(new contactgroup(
+                                 name,
+                                 alias));
+
+  try {
+    // Add new items to the configuration state.
+    configuration::applier::state::instance().contactgroups()[name] = obj;
+
+    // Notify event broker.
+    timeval tv(get_broker_timestamp(NULL));
+    broker_group(
+      NEBTYPE_CONTACTGROUP_ADD,
+      NEBFLAG_NONE,
+      NEBATTR_NONE,
+      obj.get(),
+      &tv);
+  }
+  catch (...) {
+    obj.clear();
+  }
+
+  return (obj.get());
+}
+
+/**
  * Constructor.
  */
 contactgroup::contactgroup() {}
+
+/**
+ * Constructor.
+ */
+contactgroup::contactgroup(
+                char const* name,
+                char const* alias)
+  : _name(name) {
+  if (!alias || !alias[0])
+    _alias = name;
+  else
+    _alias = alias;
+}
 
 /**
  * Copy constructor.
@@ -66,16 +138,16 @@ bool contactgroup::check(int* w, int* e) {
   int errors(0);
 
   // Check all the group members.
-  for (umap<std::string, shared_ptr<contact> >::const_iterator
-         it(cg->get_members().begin()),
-         end(cg->get_members().end());
+  for (umap<std::string, shared_ptr<contact> >::iterator
+         it(get_members().begin()),
+         end(get_members().end());
          it != end;
          ++it) {
-    contact* temp_contact(find_contact(it->first));
+    contact* temp_contact(find_contact(it->first.c_str()));
     if (!temp_contact) {
       logger(log_verification_error, basic)
-        << "Error: Contact '" << temp_contactsmember->contact_name
-        << "' specified in contact group '" << cg->get_name()
+        << "Error: Contact '" << it->first
+        << "' specified in contact group '" << get_name()
         << "' is not defined anywhere!";
       errors++;
     }
@@ -108,7 +180,7 @@ bool contactgroup::check(int* w, int* e) {
   // Check for illegal characters in contact group name.
   if (contains_illegal_object_chars()) {
     logger(log_verification_error, basic)
-      << "Error: The name of contact group '" << cg->group_name
+      << "Error: The name of contact group '" << get_name()
       << "' contains one or more illegal characters.";
     errors++;
   }
@@ -126,6 +198,58 @@ bool contactgroup::check(int* w, int* e) {
   return (errors == 0);
 }
 
-std::string& contactgroup::get_name() const {
+std::string const& contactgroup::get_name() const {
   return _name;
+}
+
+umap<std::string, shared_ptr<contact> > const& contactgroup::get_members() const {
+  return _members;
+}
+
+umap<std::string, shared_ptr<contact> >& contactgroup::get_members() {
+  return _members;
+}
+
+bool contactgroup::contains_illegal_object_chars() const {
+  if (_name.empty() || !illegal_object_chars)
+    return false;
+  return (_name.find(illegal_object_chars) != std::string::npos);
+}
+
+std::string const& contactgroup::get_alias() const {
+  return _alias;
+}
+
+void contactgroup::add_contact(std::string const& contact_name) {
+  // Make sure we have the data we need.
+  if (contact_name.empty())
+    throw (engine_error() << "Error: Contact name is empty");
+
+  _members[contact_name] = shared_ptr<contact>(0);
+
+  // Notify event broker.
+  timeval tv(get_broker_timestamp(NULL));
+  broker_group_member(
+    NEBTYPE_CONTACTGROUPMEMBER_ADD,
+    NEBFLAG_NONE,
+    NEBATTR_NONE,
+    NULL, // FIXME DBR this should be the contactmember almost empty we don't
+          // build anymore.
+    this,
+    &tv);
+}
+
+void contactgroup::clear_members() {
+  _members.clear();
+}
+
+void contactgroup::update_config(configuration::contactgroup const& obj) {
+  configuration::applier::modify_if_different(
+    _alias, obj.alias().empty() ? obj.contactgroup_name() : obj.alias());
+}
+
+bool contactgroup::contains_member(std::string const& name) const {
+  umap<std::string, shared_ptr<contact> >::const_iterator it(
+    get_members().find(name));
+  return (it != get_members().end());
 }
