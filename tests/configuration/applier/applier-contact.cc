3c167b6064618d700ae05a6fd54be6e968c531fc
David Boucher <dboucher@centreon.com>
2017-12-06 15:13:44 +0100

    test(applier::contact): New tests to cover almost all applier::contact

--- tests/configuration/applier/applier-contact.cc
+++ tests/configuration/applier/applier-contact.cc
@@ -63,9 +63,9 @@ TEST_F(ApplierContact, NewContactgroupFromConfig) {
   ASSERT_EQ(cgs.size(), 1);
 }
 
-// Given an empty name
-// When the add_contact function is called with it as argument,
-// Then it returns a NULL pointer.
+// Given contactgroup / contact appliers
+// And a configuration contactgroup and a configuration contact
+// Then the appliers add_object add the contact group and the contact.
 TEST_F(ApplierContact, NewContactFromConfig) {
   configuration::applier::contact aply;
   configuration::applier::contactgroup aply_grp;
@@ -87,4 +87,96 @@ TEST_F(ApplierContact, NewContactFromConfig) {
   ASSERT_EQ(it->first, "cmd1");
   ++it;
   ASSERT_EQ(it->first, "cmd2");
+}
+
+// Given a contact applier
+// And a configuration contact
+// When we modify the contact configuration with an unexisting contact
+// configuration
+// Then an exception is thrown.
+TEST_F(ApplierContact, ModifyUnexistingContactConfigFromConfig) {
+  configuration::applier::contact aply;
+  configuration::contact ctct("test");
+  ASSERT_TRUE(ctct.parse("contactgroups", "test_group"));
+  ASSERT_TRUE(ctct.parse("host_notification_commands", "cmd1,cmd2"));
+  ASSERT_THROW(aply.modify_object(ctct), std::exception);
+}
+
+// Given a contact applier
+// And a configuration contact
+// When we modify the contact configuration with an unexisting contact
+// Then an exception is thrown.
+TEST_F(ApplierContact, ModifyUnexistingContactFromConfig) {
+  configuration::applier::contact aply;
+  configuration::contact ctct("test");
+  ASSERT_TRUE(ctct.parse("contactgroups", "test_group"));
+  ASSERT_TRUE(ctct.parse("host_notification_commands", "cmd1,cmd2"));
+  config->contacts().insert(ctct);
+  ASSERT_THROW(aply.modify_object(ctct), std::exception);
+}
+
+// Given contactgroup / contact appliers
+// And a configuration contactgroup and a configuration contact
+// that are already in configuration
+// When we change the contact configuration
+// Then the appliers modify_object updates the contact.
+TEST_F(ApplierContact, ModifyContactFromConfig) {
+  configuration::applier::contact aply;
+  configuration::applier::contactgroup aply_grp;
+  configuration::contactgroup grp("test_group");
+  configuration::contact ctct("test");
+  ASSERT_TRUE(ctct.parse("contactgroups", "test_group"));
+  ASSERT_TRUE(ctct.parse("host_notification_commands", "cmd1,cmd2"));
+  aply_grp.add_object(grp);
+  aply.add_object(ctct);
+  aply.expand_objects(*config);
+  ASSERT_TRUE(ctct.parse("alias", "test_alias"));
+  ASSERT_TRUE(ctct.parse("host_notification_commands", "host_cmd1,host_cmd2"));
+  ASSERT_TRUE(ctct.parse("service_notification_commands", "svc_cmd1,svc_cmd2"));
+  ASSERT_TRUE(ctct.parse("_cust", "MyValue"));
+  aply.modify_object(ctct);
+  contact_map::const_iterator
+    it(configuration::applier::state::instance().contacts_find("test"));
+  ASSERT_TRUE(it->second->get_alias() == "test_alias");
+}
+
+// Given contactgroup / contact appliers
+// And a configuration contactgroup and a configuration contact
+// that are already in configuration
+// When we change the contact configuration
+// Then the appliers modify_object updates the contact.
+TEST_F(ApplierContact, RemoveContactFromConfig) {
+  configuration::applier::contact aply;
+  configuration::applier::contactgroup aply_grp;
+  configuration::contactgroup grp("test_group");
+  configuration::contact ctct("test");
+  ASSERT_TRUE(ctct.parse("contactgroups", "test_group"));
+  ASSERT_TRUE(ctct.parse("host_notification_commands", "cmd1,cmd2"));
+  ASSERT_TRUE(ctct.parse("service_notification_commands", "svc1,svc2"));
+  ASSERT_TRUE(ctct.parse("_superVar", "superValue"));
+  aply_grp.add_object(grp);
+  aply.add_object(ctct);
+  aply.expand_objects(*config);
+  aply.remove_object(ctct);
+  ASSERT_TRUE(configuration::applier::state::instance().contacts().empty());
+}
+
+// Given contactgroup / contact appliers
+// And a configuration contactgroup and a configuration contact
+// that are already in configuration
+// When we resolve the contact configuration
+// Then the contact contactgroups is cleared, nothing more if the
+// contact check is OK. Here, since notification commands are empty,
+// an exception is thrown.
+TEST_F(ApplierContact, ResolveContactFromConfig) {
+  configuration::applier::contact aply;
+  configuration::applier::contactgroup aply_grp;
+  configuration::contactgroup grp("test_group");
+  configuration::contact ctct("test");
+  ASSERT_TRUE(ctct.parse("contactgroups", "test_group"));
+  ASSERT_TRUE(ctct.parse("host_notification_commands", "cmd1,cmd2"));
+  aply_grp.add_object(grp);
+  aply.add_object(ctct);
+  aply.expand_objects(*config);
+  ASSERT_THROW(aply.resolve_object(ctct), std::exception);
 }
