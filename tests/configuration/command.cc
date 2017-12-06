ad9f5c53021984d3bb8defb1adb6ad8ec37c3fb4
David Boucher <dboucher@centreon.com>
2017-12-06 15:46:01 +0100

    tests(configuration::command): New tests on configuration::command

---
+++ tests/configuration/command.cc
@@ -0,0 +1,57 @@
+/*
+** Copyright 2017 Centreon
+**
+** This file is part of Centreon Engine.
+**
+** Centreon Engine is free software: you can redistribute it and/or
+** modify it under the terms of the GNU General Public License version 2
+** as published by the Free Software Foundation.
+**
+** Centreon Engine is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Centreon Engine. If not, see
+** <http://www.gnu.org/licenses/>.
+*/
+
+#include <memory>
+#include <gtest/gtest.h>
+#include "../timeperiod/utils.hh"
+#include "com/centreon/engine/configuration/applier/state.hh"
+#include "com/centreon/engine/configuration/command.hh"
+#include "com/centreon/engine/configuration/state.hh"
+#include "com/centreon/engine/notifications/notifier.hh"
+#include "com/centreon/shared_ptr.hh"
+
+using namespace com::centreon;
+using namespace com::centreon::engine;
+using namespace com::centreon::engine::configuration;
+using namespace com::centreon::engine::configuration::applier;
+
+extern configuration::state* config;
+
+class ConfigCommand : public ::testing::Test {
+ public:
+  void SetUp() {
+    if (config == NULL)
+      config = new configuration::state;
+    configuration::applier::state::load();  // Needed to create a contact
+  }
+
+  void TearDown() {
+    configuration::applier::state::unload();
+    delete config;
+    config = NULL;
+  }
+
+};
+
+// When I create a configuration::contact with an empty name
+// Then an exception is thrown.
+TEST_F(ConfigCommand, NewCommandWithNoName) {
+  configuration::contact ctct("");
+  ASSERT_THROW(ctct.check_validity(), std::exception);
+}
